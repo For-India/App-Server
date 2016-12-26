97f26dd40d3a311347722fbeae517eb8a2a8d95e
Pratheev <pratheev@email.com>
2016-12-26 11:40:22 +0000

    Ranjith Joins

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,73 +1,27 @@
-var User = require("../models/oa-user");
-var config = require("../../config/main");
-var jwt = require("jsonwebtoken");
+var appointmentService = require("../../config/appointmentService");
 var express = require("express");
 
 module.exports = function(app,passport){
 
+
 app.get('/', function(req,res){
     res.send("The Server is Up for India!");
 });
+        
     
-    
-    
-// Create API group routes
+// API for Online Appointment
 var oa = express.Router();
+app.use('/oa', oa);
 
-// Register new users
-oa.post('/signup', function(req, res) {
-  if(!req.body.email || !req.body.password || !req.body.name) {
-    res.json({ success: false, message: 'Please enter all the details.' });
-  } else {
-    var newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    });
-
-    // Attempt to save the user
-    newUser.save(function(err) {
-      if (err) {
-        return res.json({ success: false, message: 'That email address already exists.'});
-      }
-      res.json({ success: true, message: 'Successfully created new user.' });
-    });
-  }
-});
+// Signup
+oa.post('/signup', appointmentService.signup );
 
 // Authenticate the user and get a JSON Web Token to include in the header of future requests.
-oa.post('/login', function(req, res) {
-  User.findOne({
-    email: req.body.email
-  }, function(err, user) {
-    if (err) throw err;
-
-    if (!user) {
-      res.send({ success: false, message: 'Authentication failed. User not found.' });
-    } else {
-      // Check if password matches
-      user.comparePassword(req.body.password, function(err, isMatch) {
-        if (isMatch && !err) {
-          // Create token if the password matched and no error was thrown
-          
-          var token = jwt.sign(user, config.secret, {
-            expiresIn: 10080 // in seconds
-          });
-          res.json({ success: true, token: 'JWT ' + token });
-        } else {
-          res.send({ success: false, message: 'Authentication failed. Passwords did not match.' });
-        }
-      });
-    }
-  });
-});
+oa.post('/login', appointmentService.login);
 
 // Protect dashboard route with JWT
-oa.get('/dashboard', passport.authenticate('jwt', { session: false }), function(req, res) {
-  res.send('It worked! User id is: ' + req.user._id + '.');
-});
+oa.get('/dashboard', passport.authenticate('jwt', { session: false }), appointmentService.dashboard);
 
-// Set url for API group routes
-app.use('/oa', oa);
+
 
 };
---
+++ config/appointmentService.js
@@ -0,0 +1,57 @@
+var User = require("../models/oa-user");
+var jwt = require("jsonwebtoken");
+var config = require("../../config/main");
+var Session = require("../models/oa-session");
+
+module.signup = function(req,res){
+if(!req.body.email || !req.body.password || !req.body.name) {
+    res.json({ success: false, message: 'Please enter all the details.' });
+  } else {
+    var newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+
+    // Attempt to save the user
+    newUser.save(function(err) {
+      if (err) {
+        return res.json({ success: false, message: 'That email address already exists.'});
+      }
+      res.json({ success: true, message: 'Successfully created new user.' });
+    });
+  }
+
+};
+
+
+module.login = function(req, res) {
+  User.findOne({
+    email: req.body.email
+  }, function(err, user) {
+    if (err) throw err;
+
+    if (!user) {
+      res.send({ success: false, message: 'Authentication failed. User not found.' });
+    } else {
+      // Check if password matches
+      user.comparePassword(req.body.password, function(err, isMatch) {
+        if (isMatch && !err) {
+          // Create token if the password matched and no error was thrown
+          
+          var token = jwt.sign(user, config.secret, {
+            expiresIn: 10080 // in seconds
+          });
+          
+          res.json({ success: true, token: 'JWT ' + token });
+        } else {
+          res.send({ success: false, message: 'Authentication failed. Passwords did not match.' });
+        }
+      });
+    }
+  });
+};
+
+module.dashboard = function(req, res) {
+  res.json(req.user._id);
+};
